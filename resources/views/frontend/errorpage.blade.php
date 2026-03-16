3d2bc8c11257cd8709c91c6538642f78fa193e1c
JosphatKimani1 <githuthajosphat@gmail.com>
2026-03-16 10:25:48 +0300

    added a styling to errorpage

--- resources/views/frontend/errorpage.blade.php
+++ resources/views/frontend/errorpage.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        body{
+            background-color: darkgray
+        }
+        h1{
+            color:gold
+            
+        }
+        p{
+            color:brown
+        }
+    </style>
 </head>
 <body>
     <h1>Hello motherfuckers!</h1>
+
+    <p>The programmer for this site aliamus kuingia nyasi. Sorry guys! 😒😒🤣</p>
 </body>
 </html>
