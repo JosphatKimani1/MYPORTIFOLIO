90ebe5b4740f068643a4a9bac742608e180e8954
JosphatKimani1 <githuthajosphat@gmail.com>
2026-03-17 20:48:59 +0300

    updated dockerfile

---
+++ resources/views/frontend/errorpage.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+    <h1>Something went wrong!</h1>
+    <p>Check your Laravel logs for the underlying issue.</p>
+</body>
+</html>
